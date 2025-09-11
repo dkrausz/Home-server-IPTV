f24456ed9a747d308004b8ee9c1408916c83163c
Danilo Krausz <danilo.krausz@gmail.com>
2025-09-11 09:49:43 +0200

    Added structure in the project

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,11 @@
 import express, { json, NextFunction, Request, Response } from "express";
-import { createVideos, videosPath } from "./video";
+import { videoService } from "./videos/services/videoService";
+import path from "path";
+import multer from "multer";
+import { validateVideoMiddleware } from "./videos/middleware/validateVideo";
+import { videoRouter } from "./videos/routes/routes";
 
 export const app = express();
 
 app.use(json());
-app.use(
-  "/videos",
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log("está tentando acessar");
-    next();
-  },
-  express.static(videosPath)
-);
-
-app.get("/refresh", (req: Request, res: Response) => {
-  createVideos();
-  res.status(200).json("playlist atualizada");
-});
+app.use("/videos", videoRouter);
